ad1f6bda00c0f78e617aa4787db48de6809ef147
zhq <519662924@qq.com>
2020-11-27 17:58:08 +0800

    路由导航添加

--- src/router.js
+++ src/router.js
@@ -1,3 +1,34 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from './pages/home';
+import Index from './pages/index';
+import Product from './pages/product';
+import Detail from './pages/detail';
+Vue.use(VueRouter);
 
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children:[
+        {
+          path: '/index',
+          name: 'index',
+          component: Index
+        },
+        {
+          path: '/product',
+          name: 'product',
+          component: Product
+        },
+        {
+          path: '/detail',
+          name: 'detail',
+          component: Detail
+        }
+      ]
+    }
+  ]
+})
