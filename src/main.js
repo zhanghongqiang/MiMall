c10136f1e0a5799dd0aa0141716fd42eade82dc8
zhq <519662924@qq.com>
2020-11-28 15:43:02 +0800

    axios响应拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios  from 'axios'
+
+Vue.use(VueAxios,axios)
+
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 5000
+
+axios.interceptors.response.use = function(response){
+  let res = response.data
+  if(res.status == 0){
+    return res.data
+  }else if(res.status == 10){
+    window.location.href = '/#/login'
+  }else{
+    alert(res.msg)
+  }
+}
 new Vue({
   router,
   render: h => h(App),
