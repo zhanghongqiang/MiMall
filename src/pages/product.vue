418c9e8c0aea924cca63b60964a15a9533d200ed
zhq <519662924@qq.com>
2020-12-15 18:09:37 +0800

    吸顶弹窗效果

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>Product</div>
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <div class="btn">立即购买</div>
+      </template>
+    </product-param>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+  </div>
 </template>
 
 <script>
+import ProductParam from './../components/ProductParam'
 export default {
-  name: 'product'
+  name: 'product',
+  components:{
+    ProductParam
+  }
 }
 </script>
 
-<style>
-
+<style lang='scss'>
+  .product{
+    .h{
+      height: 100px;
+      line-height: 100px;
+    }
+    .btn{
+      margin-left: 10px;
+    }
+  }
 </style>
